615580863fb97e485f53139f4bbf9b96ddb8f3a9
Jacobo Forero <dani.jacobof06@gmail.com>
2025-11-21 15:12:33 -0500

    drop *

--- Static/Js/partners.js
+++
@@ -1,9 +0,0 @@
-const partnersTrack = document.querySelector('.partners-track');
-
-partnersTrack.addEventListener('mouseenter', () => {
-  partnersTrack.style.animationPlayState = 'paused';
-});
-
-partnersTrack.addEventListener('mouseleave', () => {
-  partnersTrack.style.animationPlayState = 'running';
-});
